78859ff3ff62b1762e8879d33010d63f470aac75
Suhel Ahamd <suhelnid@gmail.com>
2018-06-30 17:27:35 +0530

    path change

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var routes_config_1 = require("./config/routes.config");
 var db_config_1 = require("./config/db.config");
 var passport_config_1 = require("./config/passport.config");
 var index_1 = require("./routes/index");
+var path = require("path");
 require('dotenv').config();
 var app = express();
 global['dbConn'] = mongoose.createConnection(process.env.PRODUCTION);
@@ -15,8 +16,9 @@ if (global['dbConn']) {
     routes_config_1.RoutesConfig.init(app);
     index_1.Routes.init(app, express.Router());
     passport_config_1.PassportConfig.init();
-    app.get('/*', function (req, res) {
-        res.sendFile('/app/dist/index.html');
+    app.get('*', function (req, res) {
+        var index = path.join(__dirname, 'build', 'index.html');
+        res.sendFile(index);
     });
     http.createServer(app).listen(process.env.PORT || 8080, function () {
         console.log('starting server..');
--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import {RoutesConfig} from './config/routes.config';
 import {DbConfig} from "./config/db.config";
 import {PassportConfig} from "./config/passport.config";
 import {Routes} from './routes/index';
+import * as path from "path";
 require('dotenv').config();
 
 
@@ -15,8 +16,9 @@ if(global['dbConn']) {
   RoutesConfig.init(app);
   Routes.init(app, express.Router())
   PassportConfig.init();
-  app.get('/*',function (req,res) {
-    res.sendFile('/app/dist/index.html')
+  app.get('*',function (req,res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
   })
   http.createServer(app).listen(process.env.PORT || 8080, function () {
     console.log('starting server..')
